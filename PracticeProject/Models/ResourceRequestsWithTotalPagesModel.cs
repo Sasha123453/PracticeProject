458db1b538616d019f75010a13f9f329f0540403
Саша Шелковин <shelkovin.54@gmail.com>
2023-07-09 14:20:37 +0300

    .

---
+++ PracticeProject/Models/ResourceRequestsWithTotalPagesModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PracticeProject.Models
+{
+    public class ResourceRequestsWithTotalPagesModel
+    {
+        public IQueryable<ResourceRequestWithNicknameModel> Requests;
+        public int TotalPages { get; set; }
+    }
+}
